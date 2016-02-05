c7cd71c74ace944026a3955b84a0b27dcd4d5132
jonahjordan1992 <jonah.jordan@student.kdg.be>
2016-02-05 19:14:35 +0100

    Eerste commit

---
+++ BeatBuddy/BeatBuddy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeatBuddy.MvcApplication" Language="C#" %>
---
+++ BeatBuddy/BeatBuddy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeatBuddy.Startup))]
+namespace BeatBuddy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
