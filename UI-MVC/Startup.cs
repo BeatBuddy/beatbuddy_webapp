f7909c7960de5ade6adb9d76d8c71873d3c403fc
jonahjordan1992 <jonah.jordan@student.kdg.be>
2016-02-06 10:54:05 +0100

    First Commit

---
+++ UI-MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BB.UI.Web.MVC.Startup))]
+namespace BB.UI.Web.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
