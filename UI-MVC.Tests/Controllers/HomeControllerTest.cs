e262d84af0a39e8998176b64164d96f587db8a77
Maarten <maarten.vangiel@student.kdg.be>
2016-02-06 14:10:54 +0100

    Removed unnecessary code, Removed bootstrap, Added bootstrap-grid, Now using sass for stylesheet, Disabled CustomErrors so YSOD is shown

--- UI-MVC.Tests/Controllers/HomeControllerTest.cs
+++ UI-MVC.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using BB.UI.Web.MVC.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using BB.UI.Web.MVC;
 
 namespace BB.UI.Web.MVC.Tests.Controllers
 {
@@ -15,39 +10,8 @@ namespace BB.UI.Web.MVC.Tests.Controllers
         [TestMethod]
         public void Index()
         {
-            // Arrange
             HomeController controller = new HomeController();
-
-            // Act
             ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
             Assert.IsNotNull(result);
         }
     }
