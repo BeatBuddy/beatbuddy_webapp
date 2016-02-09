24289afa7090402d14885c758bd867631e34bb75
jonahjordan1992 <jonah.jordan@student.kdg.be>
2016-02-09 13:17:34 +0100

    Added few changes

--- Domain/Playlists/Vote.cs
+++ Domain/Playlists/Vote.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BB.BL.Domain.Playlists;
+﻿using BB.BL.Domain.Users;
 
-namespace BB.BL.Domain.Users
+namespace BB.BL.Domain.Playlists
 {
     public class Vote
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public int Score { get; set; }
         public User User { get; set; }
     }
--- Domain/Users/User.cs
+++ Domain/Users/User.cs
@@ -11,7 +11,7 @@ namespace BB.BL.Domain.Users
 {
     public class User
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string LastName { get; set; }
