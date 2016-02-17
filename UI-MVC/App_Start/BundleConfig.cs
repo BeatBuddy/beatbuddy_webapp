b5bdbaf13c66598f774c5a794643b04b8cb90562
jonahjordan1992 <jonah.jordan@student.kdg.be>
2016-02-17 20:47:33 +0100

    Loginview aangepast

--- UI-MVC/App_Start/BundleConfig.cs
+++ UI-MVC/App_Start/BundleConfig.cs
@@ -8,8 +8,9 @@ namespace BB.UI.Web.MVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery")
-                        .Include("~/Scripts/jquery-{version}.js")
-                        .Include("~/Scripts/jquery.tmpl.min.js"));
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.tmpl.min.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
