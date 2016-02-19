6ee90760c19102370fa5c025710b892abbce4e82
acidshards <matthiasheylen@hotmail.com>
2016-02-19 15:56:32 +0100

    Bearer token authentication in Web API (login) :shipit: //pair programming with @ignacemaes

--- Domain/Playlists/Playlist.cs
+++ Domain/Playlists/Playlist.cs
@@ -20,8 +20,8 @@ namespace BB.BL.Domain.Playlists
         public int MaximumVotesPerUser { get; set; }
         public bool Active { get; set; }
         public string ImageUrl { get; set; }
-        public long PlaylistMasterId { get; set; }
-        public long CreatedById { get; set; }
+        public long? PlaylistMasterId { get; set; }
+        public long? CreatedById { get; set; }
         public Collection<PlaylistTrack> PlaylistTracks { get; set; }
         public Collection<Comment> Comments { get; set; }
         public Collection<Comment> ChatComments { get; set; }
