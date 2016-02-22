e7eb4742193cc91ac106489e47ff5c4b4abaa36d
acidshards <matthiasheylen@hotmail.com>
2016-02-22 16:01:39 +0100

    :hope:

--- UI-MVC.Tests/Controllers/WebApi/UserControllerTest.cs
+++ UI-MVC.Tests/Controllers/WebApi/UserControllerTest.cs
@@ -21,30 +21,10 @@ namespace BB.UI.Web.MVC.Tests.Controllers.WebApi
             DbInitializer.Initialize();
         }
 
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
         [TestMethod]
         public void TestMethod1()
         {
-            //
-            // TODO: Add test logic here
-            //
+            //controller
         }
     }
 }
