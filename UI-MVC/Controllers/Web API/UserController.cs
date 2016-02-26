b6e3cd8c2ec5dde915c69f78407ad4c29abefb26
acidshards <matthiasheylen@hotmail.com>
2016-02-26 17:20:47 +0100

    Implemented registering via WebAPI  :beers:

--- UI-MVC/Controllers/Web API/UserController.cs
+++ UI-MVC/Controllers/Web API/UserController.cs
@@ -1,6 +1,15 @@
 ﻿using BB.BL;
 using BB.BL.Domain;
+using BB.UI.Web.MVC.Models;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Web;
+using System.Threading.Tasks;
+using BB.BL.Domain.Users;
+using System;
+using System.Net;
 
 namespace BB.UI.Web.MVC.Controllers.Web_API
 {
@@ -9,8 +18,9 @@ namespace BB.UI.Web.MVC.Controllers.Web_API
     {
         private readonly IUserManager userManager;
         private readonly IOrganisationManager organisationManager;
+        private ApplicationUserManager _userManager;
 
-  
+
         public UserController()
         {
             userManager = new UserManager(ContextEnum.BeatBuddy);
@@ -21,7 +31,31 @@ namespace BB.UI.Web.MVC.Controllers.Web_API
             userManager = new UserManager(contextEnum);
             organisationManager = new OrganisationManager(contextEnum);
         }
-    
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("register")]
+        public async Task<IHttpActionResult> Register([FromUri] string firstName, [FromUri] string lastName, [FromUri] string nickname, [FromUri] string email, [FromUri] string password)
+        {
+            User user;
+            try
+            {
+                user = userManager.CreateUser(email, lastName, firstName, nickname, "");
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.InternalServerError, ex.GetBaseException().Message);
+            }
+            var applicationUser = new ApplicationUser { UserName = email, Email = email };
+            var resultUser = await UserManager.CreateAsync(applicationUser, password);
+            if (resultUser.Succeeded)
+            {
+                UserManager.AddToRole(applicationUser.Id, "User");
+                return Ok();
+            }
+            return NotFound ();
+        }
+
 
         // GET: api/users/heylenmatthias@gmail.com
         [Authorize]
@@ -52,6 +86,18 @@ namespace BB.UI.Web.MVC.Controllers.Web_API
             var organisations = organisationManager.ReadOrganisations(id);
 
             return Ok(organisations);
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
 
     }
