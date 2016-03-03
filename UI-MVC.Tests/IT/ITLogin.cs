d6c9ca8fe142359c3b184b4b80db880d50140f8c
acidshards <matthiasheylen@hotmail.com>
2016-03-03 14:34:52 +0100

    implemented test for log out and failed login

--- UI-MVC.Tests/IT/ITLogin.cs
+++ UI-MVC.Tests/IT/ITLogin.cs
@@ -2,6 +2,9 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using BB.UI.Web.MVC.Controllers;
+using Microsoft.Web.Administration;
+using System.Linq;
+using System;
 
 namespace BB.UI.Web.MVC.Tests.IT
 {
@@ -15,6 +18,8 @@ namespace BB.UI.Web.MVC.Tests.IT
         {
             chromeDriver = new ChromeDriver();
             AccountController _acountController = new AccountController();
+           
+         
         }
 
         [TestMethod]
@@ -37,7 +42,52 @@ namespace BB.UI.Web.MVC.Tests.IT
             loginButton.Click();
             var helloMessage = chromeDriver.FindElement(By.XPath("//a[@href='/Manage']"));
             Assert.AreEqual("hello admin@admin.com!", helloMessage.Text);
+
+            var myPortal = chromeDriver.FindElement(By.XPath("//a[@href='/Home/Portal']"));
+            Assert.AreEqual("my portal", myPortal.Text);
+
+           
+            var logOff = chromeDriver.FindElement(By.LinkText("log off"));
+            logOff.Click();
+
+            chromeDriver.FindElement(By.XPath("//a[@href='#loginModal']")).Click();
+
+            var login = chromeDriver.FindElement(By.XPath("//a[@href='#loginModal']"));
+            Assert.IsTrue(login.Displayed);
+            Assert.AreEqual("login", login.Text);
+            
         }
+
+        [TestMethod]
+        public void LoginFailedTest() {
+            chromeDriver.Navigate().GoToUrl("http://teamd.azurewebsites.net/");
+            var loginModal = chromeDriver.FindElement(By.Id("loginModal"));
+
+            Assert.IsFalse(loginModal.Displayed);
+
+            chromeDriver.FindElement(By.XPath("//a[@href='#loginModal']")).Click();
+
+            loginModal = chromeDriver.FindElement(By.Id("loginModal"));
+            Assert.IsTrue(loginModal.Displayed);
+
+            loginModal.FindElement(By.Id("Email")).SendKeys("admin@admin.com");
+            loginModal.FindElement(By.Id("Password")).SendKeys("wrongpassword");
+
+            var loginButton = chromeDriver.FindElement(By.XPath("//input[@value='Login']"));
+            loginButton.Click();
+
+            var login = chromeDriver.FindElement(By.XPath("//form[@action='/Account/Login']"));
+
+            var error = login.FindElements(By.XPath("//div[@class='validation-summary-errors text-danger']"));
+            foreach(var errorr in error) {
+                if (errorr.Displayed) {
+                    var errorMessage = errorr.FindElement(By.TagName("ul")).FindElement(By.TagName("li"));
+                    Assert.AreEqual("Invalid login attempt.", errorMessage.Text);
+                }
+            }
+        }
+
+
 
         [TestCleanup]
         public void TestCleanup() {
