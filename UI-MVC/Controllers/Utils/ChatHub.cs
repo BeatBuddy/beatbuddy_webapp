090d5f9349d1cee9b9ca6b13fa41f7c931350487
kelly Mdp <kelly.metdepenningen@skynet.be>
2016-03-08 19:38:04 +0100

    Chat

---
+++ UI-MVC/Controllers/Utils/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+
+namespace BB.UI.Web.MVC.Controllers.Utils
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.BroadcastMessage(name, message);
+        }
+    }
+}
