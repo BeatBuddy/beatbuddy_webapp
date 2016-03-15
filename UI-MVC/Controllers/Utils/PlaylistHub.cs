548834b9ef0d167231d6f3c8bae44121afa9596f
Maarten <maarten.vangiel@student.kdg.be>
2016-03-15 18:57:54 +0100

    Refactorings :boom: :fire:

--- UI-MVC/Controllers/Utils/PlaylistHub.cs
+++ UI-MVC/Controllers/Utils/PlaylistHub.cs
@@ -1,29 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using BB.BL;
 using BB.BL.Domain;
-using BB.BL.Domain.Playlists;
-using BB.BL.Domain.Users;
 using BB.DAL;
 using BB.DAL.EFUser;
 using BB.UI.Web.MVC.Models;
 using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
 using VideoLibrary;
-
 
 namespace BB.UI.Web.MVC.Controllers.Utils
 {
     public class PlaylistHub : Hub
     {
-        private static Dictionary<string, CurrentListenerModel> connectedGroupUsers = new Dictionary<string, CurrentListenerModel>(); 
-        private static Dictionary<string, string> playlistMasters = new Dictionary<string, string>();
-        private static Dictionary<string, string> lastJoiner = new Dictionary<string, string>();
-        private static UserManager userManager = new UserManager(new UserRepository(new EFDbContext(ContextEnum.BeatBuddy)));
+        private static readonly Dictionary<string, CurrentListenerModel> connectedGroupUsers = new Dictionary<string, CurrentListenerModel>();
+        private static readonly Dictionary<string, string> playlistMasters = new Dictionary<string, string>();
+        private static readonly Dictionary<string, string> lastJoiner = new Dictionary<string, string>();
+        private static readonly UserManager userManager = new UserManager(new UserRepository(new EFDbContext(ContextEnum.BeatBuddy)));
+
         public void AddTrack(string groupName)
         {
             Thread.Sleep(1000);
@@ -38,8 +33,7 @@ namespace BB.UI.Web.MVC.Controllers.Utils
             }
             lastJoiner.Add(groupName, Context.ConnectionId);
             Groups.Add(Context.ConnectionId, groupName);
-            var model = new CurrentListenerModel();
-            model.GroupName = groupName;
+            var model = new CurrentListenerModel { GroupName = groupName };
             if (Context.User != null)
             {
                 var user = userManager.ReadUser(Context.User.Identity.Name);
@@ -56,7 +50,7 @@ namespace BB.UI.Web.MVC.Controllers.Utils
             {
                 connectedGroupUsers.Remove(Context.ConnectionId);
             }
-                connectedGroupUsers.Add(Context.ConnectionId, model);
+            connectedGroupUsers.Add(Context.ConnectionId, model);
 
             Clients.Caller.modifyListeners(connectedGroupUsers.Values.ToList().FindAll(p => p.GroupName == model.GroupName).Count + " party people attending", connectedGroupUsers.Values);
             Clients.OthersInGroup(groupName).modifyListeners(connectedGroupUsers.Values.ToList().FindAll(p => p.GroupName == model.GroupName).Count + " party people attending", connectedGroupUsers.Values);
@@ -78,6 +72,7 @@ namespace BB.UI.Web.MVC.Controllers.Utils
                 Clients.Client(playlistMasters.Single(p => p.Key == groupName).Value).syncLive();
             }
         }
+
         public override Task OnConnected()
         {
             //Clients.Caller.joinGroup();
@@ -89,17 +84,15 @@ namespace BB.UI.Web.MVC.Controllers.Utils
             var key = connectedGroupUsers.Keys.Single(p => p.Equals(Context.ConnectionId));
             var model = connectedGroupUsers.FirstOrDefault(f => f.Key.Equals(Context.ConnectionId)).Value;
             if (playlistMasters.Values.Any(p => p.Equals(Context.ConnectionId)))
-                {
-                    Clients.OthersInGroup(model.GroupName).stopMusicPlaying();
-                    playlistMasters.Remove(model.GroupName);
-                }
-            
+            {
+                Clients.OthersInGroup(model.GroupName).stopMusicPlaying();
+                playlistMasters.Remove(model.GroupName);
+            }
+
             connectedGroupUsers.Remove(key);
             Clients.Group(model.GroupName).modifyListeners(connectedGroupUsers.Values.ToList().FindAll(p => p.GroupName == model.GroupName).Count + " party people attending", connectedGroupUsers.Values);
             return base.OnDisconnected(stopCalled);
         }
-
-       
 
         public void StartPlaying(CurrentPlayingViewModel track, string groupName)
         {
@@ -107,6 +100,7 @@ namespace BB.UI.Web.MVC.Controllers.Utils
             {
                 playlistMasters.Remove(groupName);
             }
+
             playlistMasters.Add(groupName, Context.ConnectionId);
 
             Clients.Client(lastJoiner.Single(p => p.Key == groupName).Value).playLive(track, 0);
@@ -115,12 +109,11 @@ namespace BB.UI.Web.MVC.Controllers.Utils
             Clients.OthersInGroup(groupName).onPlaylinkGenerated(video.Uri);
         }
 
-        
-
         public void PausePlaying(string groupName)
         {
             Clients.OthersInGroup(groupName).pauseMusicPlaying();
         }
+
         public void ResumePlaying(float duration, string groupName)
         {
             Clients.OthersInGroup(groupName).resumeMusicPlaying(duration);
