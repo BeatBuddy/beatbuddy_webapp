52c62bc393d2aec12093b3182c1fd14d88e3fb26
jonahjordan1992 <jonah.jordan@student.kdg.be>
2016-03-17 19:51:33 +0100

    kleine dingen fixen

---
+++ UI-MVC/Scripts/gridview.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    var $container = $('#ib-container'),
+        $articles = $container.children('article'),
+        timeout;
+
+    $articles.on('mouseenter', function (event) {
+
+        var $article = $(this);
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+
+            if ($article.hasClass('active')) return false;
+
+            $articles.not($article.removeClass('blur').addClass('active'))
+                     .removeClass('active')
+                     .addClass('blur');
+
+        }, 65);
+
+    });
+
+    $container.on('mouseleave', function (event) {
+
+        clearTimeout(timeout);
+        $articles.removeClass('active blur');
+
+    });
+
+});
