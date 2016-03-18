5d871c36b0958dd232b209182e0a964de9c838a1
acidshards <matthiasheylen@hotmail.com>
2016-03-18 13:34:10 +0100

    Added some OrganisationControllerTests and PlaylistController tests

--- UI-MVC.Tests/Controllers/WebApi/OrganisationsControllerTest.cs
+++ UI-MVC.Tests/Controllers/WebApi/OrganisationsControllerTest.cs
@@ -1,7 +1,18 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using BB.BL;
+using BB.BL.Domain;
+using BB.BL.Domain.Organisations;
+using BB.BL.Domain.Playlists;
+using BB.BL.Domain.Users;
+using BB.DAL;
+using BB.DAL.EFOrganisation;
+using BB.DAL.EFUser;
+using BB.UI.Web.MVC.Controllers.Web_API;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyTested.WebApi;
+using MyTested.WebApi.Builders.Contracts.Controllers;
 
 namespace BB.UI.Web.MVC.Tests.Controllers.WebApi
 {
@@ -9,48 +20,51 @@ namespace BB.UI.Web.MVC.Tests.Controllers.WebApi
     [TestClass]
     public class OrganisationsControllerTest
     {
-        /*PlaylistManager playlistManager;
         UserManager userManager;
         OrganisationManager organisationManager;
         User user;
-        Playlist playlist;
         Organisation organisation;
+
+        IAndControllerBuilder<OrganisationsController> _organisationControllerWithAuthenticatedUser;
+
 
         [TestInitialize]
         public void Initialize()
         {
-
-            playlistManager = new PlaylistManager(new PlaylistRepository(new EFDbContext(ContextEnum.BeatBuddyTest)), new UserRepository(new EFDbContext(ContextEnum.BeatBuddyTest)));
             userManager = new UserManager(new UserRepository(new EFDbContext(ContextEnum.BeatBuddyTest)));
             organisationManager = new OrganisationManager(new OrganisationRepository(new EFDbContext(ContextEnum.BeatBuddyTest)));
             user = userManager.CreateUser("werknu@gmail.com", "matthias", "test", "acidshards", "");
 
-            playlist = playlistManager.CreatePlaylistForUser("testplaylist", "gekke playlist om te testen", "125", 5, true, "", user);
+            _organisationControllerWithAuthenticatedUser = MyWebApi.Controller<OrganisationsController>()
+               .WithResolvedDependencyFor<IUserManager>(userManager)
+               .WithResolvedDependencyFor<IOrganisationManager>(organisationManager)
+               .WithAuthenticatedUser(
+                u => u.WithIdentifier("NewId")
+                      .WithUsername(user.Email)
+                      .WithClaim(new System.Security.Claims.Claim(System.Security.Claims.ClaimTypes.Email, user.Email))
+                      .WithClaim(new System.Security.Claims.Claim("sub", user.Email))
+               );
 
-            Track track = new Track()
-            {
-                Artist = "Metallica",
-                Title = "Master of Puppets (live)",
-                CoverArtUrl = "",
-                Duration = 800,
-                TrackSource = new TrackSource
-                {
-                    SourceType = SourceType.YouTube,
-                    Url = "https://www.youtube.com/watch?v=kV-2Q8QtCY4"
-                }
-            };
-            Track addedtrack = playlistManager.AddTrackToPlaylist(playlist.Id, track);
+           organisation =  organisationManager.CreateOrganisation("testorganisatie", "", user);
 
-
-            organisation = organisationManager.CreateOrganisation("gek organisatie test", "", "", user);
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void GetOrganisationTest()
         {
-            
+            _organisationControllerWithAuthenticatedUser
+                .Calling(o => o.Get(organisation.Id))
+                .ShouldReturn()
+                .Ok();
         }
 
-    */
+        [TestCleanup]
+        public void Cleanup()
+        {
+            organisationManager.DeleteOrganisation(organisation.Id);
+            userManager.DeleteUser(user.Id);
+        }
+
+
     }
 }
--- UI-MVC/Controllers/Web API/PlaylistController.cs
+++ UI-MVC/Controllers/Web API/PlaylistController.cs
@@ -401,7 +401,7 @@ namespace BB.UI.Web.MVC.Controllers.Web_API
         public IHttpActionResult Upvote(long id, long trackId)
         {
             var userIdentity = RequestContext.Principal.Identity as ClaimsIdentity;
-            var user = getUser(userIdentity);
+            var user = GetUser(userIdentity);
             var createVote = playlistManager.CreateVote(1, user.Id, trackId);
 
             var playlistTrack = playlistManager.ReadPlaylistTrack(trackId);
@@ -423,7 +423,7 @@ namespace BB.UI.Web.MVC.Controllers.Web_API
         public IHttpActionResult Downvote(long id, long trackId)
         {
             var userIdentity = RequestContext.Principal.Identity as ClaimsIdentity;
-            var user = getUser(userIdentity);
+            var user = GetUser(userIdentity);
             var createVote = playlistManager.CreateVote(-1, user.Id, trackId);
 
             var playlistTrack = playlistManager.ReadPlaylistTrack(trackId);
@@ -440,7 +440,7 @@ namespace BB.UI.Web.MVC.Controllers.Web_API
             return Ok(createVote);
         }
 
-        private User getUser(ClaimsIdentity claimsIdentity)
+        private User GetUser(ClaimsIdentity claimsIdentity)
         {
             if (claimsIdentity == null) return null;
 
